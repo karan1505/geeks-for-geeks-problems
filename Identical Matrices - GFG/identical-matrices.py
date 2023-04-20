c85a86ccd1ea341c70a0dd7f9c43885f02d7e2c7
Karan Sreedhar <79150373+karan1505@users.noreply.github.com>
2023-04-20 15:26:08 +0530

    Added solution - LeetHub

---
+++ Identical Matrices - GFG/identical-matrices.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def areMatricesidentical(self,N,Grid1,Grid2):
+        nope = 0
+        for i in range(N):
+            for j in range(N):
+                if(Grid1[i][j]==Grid2[i][j]):
+                    continue
+                else:
+                    nope = 1
+                    break
+        if(nope==1):
+            return 0
+        else:
+            return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N=int(input())
+        Grid1 = [[0 for i in range(N)] for j in range(N)]
+        for i in range(N):
+            s=list(map(int,input().strip().split(' ')))
+            for j in range(N):
+                Grid1[i][j]=s[j]
+        Grid2 = [[0 for i in range(N)] for j in range(N)]
+        for i in range(N):
+            s=list(map(int,input().strip().split(' ')))
+            for j in range(N):
+                Grid2[i][j]=s[j]        
+        ob=Solution()
+        print(ob.areMatricesidentical(N,Grid1,Grid2))
+# } Driver Code Ends
