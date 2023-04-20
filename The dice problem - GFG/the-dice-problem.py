dfbae902ca48e0e11558cef99f104d5ebb2c86c3
Karan Sreedhar <79150373+karan1505@users.noreply.github.com>
2023-04-20 16:52:57 +0530

    Added solution - LeetHub

---
+++ The dice problem - GFG/the-dice-problem.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def oppositeFaceOfDice(self, N):
+    	if(N==6):
+    	    return 1
+    	elif(N==5):
+    	    return 2
+    	elif(N==4):
+    	    return 3
+    	elif(N==3):
+    	    return 4
+    	elif(N==2):
+    	    return 5
+    	elif(N==1):
+    	    return 6
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.oppositeFaceOfDice(N)
+        print(ans)
+# } Driver Code Ends
