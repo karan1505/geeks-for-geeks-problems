ee5a562bd0ebf833f2a669859f919c0a6e778b09
Karan Sreedhar <79150373+karan1505@users.noreply.github.com>
2023-04-21 08:44:35 +0530

    Added solution - LeetHub

--- Replace all 0's with 5 - GFG/replace-all-0s-with-5.py
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.py
@@ -1,30 +1,26 @@
-#function should return an integer
-#your task is to compplete this function
-
+# Function should return an integer value
 def list_to_num(num_list):
     num_str = ''.join(map(str, num_list))
     return int(num_str)
-    
-class Solution:
-    def convertFive(self,n):
-        num = str(n)
-        changednum = [0]*len(num)
-        for i in range(0,len(num)):
-            if(num[i]!="0"):
-                changednum[i]=num[i]
-            else:
-                changednum[i]="5"
-        ans = list_to_num(changednum)
+
+def convertFive(n):
+    num = str(n)
+    changednum = [0]*len(num)
+    for i in range(0,len(num)):
+        if(num[i]!="0"):
+            changednum[i]=num[i]
+        else:
+            changednum[i]="5"
+    ans = list_to_num(changednum)
             
-        return ans
+    return ans
+
 
 #{ 
  # Driver Code Starts
+# Your code goes here
 if __name__=='__main__':
     t = int(input())
     for i in range(t):
-        n=int(input())
-        print(Solution().convertFive(n))
-# Contributed by: Harshit sidhwa
-
+        print (convertFive(int(input().strip())))
 # } Driver Code Ends
